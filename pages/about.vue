750de47800e8edc16ea996abd8dff57efcc9f163
premsaitoram <t.prem256@gmail.com>
2024-02-21 18:15:05 +0530

    add files

---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+
+            <h2>About</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa asperiores maiores quasi deleniti ipsam quis, voluptatum rerum autem nesciunt aliquam.</p>
+        </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2{
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+      aspernatur et vitae odit, provident praesentium explicabo dolores.
+      Perferendis, quas explicabo.
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi
+      aspernatur quo blanditiis perspiciatis non, facilis nisi nam fugit vel
+      impedit rem esse quam accusantium assumenda, aut quibusdam ipsa repellat
+      doloremque!
+    </p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
